38ae0697e58095abbfc53fd346a2239a9cf0dc73
DESKTOP-38JM5KT\arnau.mora_modelical <arnaumoracoll@gmail.com>
2023-12-20 12:57:11 +0100

    Inicio reto3

---
+++ Reto3.js
@@ -0,0 +1,25 @@
+/*
+Recibes un paquete de regalos navideños que Papá Noel quiere entregar a los niños. 
+Cada regalo dentro del pack está representado por un hilo y tiene un peso igual al 
+número de letras de su nombre. El trineo de Papá Noel sólo puede llevar un límite de
+peso .
+Papá Noel también tiene una lista de renos que pueden ayudarle a entregar los regalos.
+ Cada reno tiene un límite de peso máximo que puede llevar. El límite máximo de peso 
+ de cada reno es igual al doble del número de letras de su nombre .
+Tu tarea es implementar una función distributeGifts(packOfGifts, reindeers)que reciba 
+un paquete de regalos y una lista de renos y devuelva la cantidad máxima de paquetes de regalos que Papá Noel puede entregar. No puedes dividir paquetes de regalos .
+
+const packOfGifts = ["book", "doll", "ball"]
+const reindeers = ["dasher", "dancer"]
+
+// pack weights 4 + 4 + 4 = 12
+// reindeers can carry (2 * 6) + (2 * 6) = 24
+distributeGifts(packOfGifts, reindeers) // 2
+*/
+
+function distributedGifts(packOfGifts,reindeers){
+    
+
+
+    return
+}
